fadb0b54c112b3922e535830b6c65a722befbbf9
jonahwilliams <jonahwilliams@google.com>
2020-08-10 15:30:48 -0700

    publishable null-safety release

--- test/fake_platform_test.dart
+++ test/fake_platform_test.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// @dart = 2.10
 import 'dart:io' as io;
 
 import 'package:platform/platform.dart';
@@ -28,8 +29,8 @@ void _expectPlatformsEqual(Platform actual, Platform expected) {
 
 void main() {
   group('FakePlatform', () {
-    FakePlatform fake;
-    LocalPlatform local;
+    late FakePlatform fake;
+    late LocalPlatform local;
 
     setUp(() {
       fake = new FakePlatform();
